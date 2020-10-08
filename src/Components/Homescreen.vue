86b8342fe12d477b11f831d7be2e7d819fe501f2
Luke Sparrow <lukesparrow@pop-os.localdomain>
2020-10-07 20:42:26 -0600

    initial files for DnD functionality

--- src/Components/Homescreen.vue
+++ src/Components/Homescreen.vue
@@ -1,15 +1,20 @@
 <template>
 <h1>Home Screen</h1>
-<draggable v-model="movieList" @start="drag=true" @end="drag=false">
-    <div v-for="movie in movieList" :key="movie.id">{{movie.name}}</div>
-</draggable>
+<div class="container">
+    <div v-for="movie in movieList" :key="movie.id" class="draggable">{{movie.name}}</div>
+</div>
+<div class="container">
+    <div v-for="movie in ratedList" :key="movie.id" class="draggable">{{movie.name}}</div>
+</div>
 <button @click="$emit('set-location-to-sign-in')">Sign Out</button>
 <button @click="$emit('set-location-to-logged-in')">Change Ranking</button>
 </template>
 
 <script>
+import './homeScreen.js'
 import draggable from 'vuedraggable'
 import {
+    onMounted,
     reactive
 } from 'vue'
 
@@ -35,14 +40,43 @@ export default {
             }
         ])
 
+        const ratedList = reactive([{
+            id: 4,
+            name: 'Face Off',
+            rating: 4
+        }])
+
+        onMounted(() => {
+            let externalScript = document.createElement('script')
+            externalScript.setAttribute('src', './homeScreen.js')
+            document.head.appendChild(externalScript)
+        })
+
         return {
             draggable,
-            movieList
+            movieList,
+            ratedList
         }
     }
 }
 </script>
 
 <style scoped>
+.container {
+    background-color: #333;
+    padding: 1rem;
+    margin-top: 1rem;
+}
 
+.draggable {
+    padding: 1rem;
+    margin: 5px;
+    background-color: white;
+    border: 1px solid black;
+    cursor: move;
+}
+
+.draggable.dragging {
+    opacity: .5;
+}
 </style>
--- src/Components/LoggedIn.vue
+++ src/Components/LoggedIn.vue
@@ -1,10 +1,10 @@
 <template>
 <h1>Logged In!</h1>
 <div class="drop-zone" @drop="onDrop($event, 1)" @dragover.prevent @dragenter.prevent>
-    <div v-for="item in listOne" :key="item.id" class="drag-el" draggable="true" @dragstart="startDrag($event, item)">{{ item.title }}</div>
+    <div v-for="item in listOne" :key="item.id" class="drag-el" :class="'toggleDrag'" draggable="true" @dragstart="startDrag($event, item)">{{ item.title }}</div>
 </div>
 <div class="drop-zone" @drop="onDrop($event, 2)" @dragover.prevent @dragenter.prevent>
-    <div v-for="item in listTwo" :key="item.id" class="drag-el" draggable="true" @dragstart="startDrag($event, item)">{{ item.title }}</div>
+    <div v-for="item in listTwo" :key="item.id" class="drag-el" :class="{'dragging': isDragging}" draggable="true" @dragstart="startDrag($event, item)">{{ item.title }}</div>
 </div>
 <button @click="$emit('set-location-to-sign-in')">Sign Out</button>
 <button @click="$emit('set-location-to-home-screen')">Home Screen</button>
@@ -13,13 +13,17 @@
 <script>
 import {
     computed,
-    reactive
+    reactive,
+    ref
 } from 'vue'
 
 export default {
     name: 'LoggedIn',
 
     setup() {
+
+        const isDragging = ref(false)
+
         let items = reactive([{
                 id: 1,
                 title: 'Item 1',
@@ -41,6 +45,7 @@ export default {
         const listTwo = computed(() => items.filter(item => item.list === 2))
 
         function startDrag(evt, item) {
+            isDragging.value = !isDragging.value
             evt.dataTransfer.dropEffect = 'move'
             evt.dataTransfer.effectAllowed = 'move'
             evt.dataTransfer.setData('itemID', item.id)
@@ -48,6 +53,7 @@ export default {
         }
 
         function onDrop(evt, list) {
+            isDragging.value = !isDragging.value
             const itemID = parseInt(evt.dataTransfer.getData('itemID'))
             const item = this.items.find(item => item.id === itemID)
             item.list = list
@@ -59,7 +65,8 @@ export default {
             listOne,
             listTwo,
             startDrag,
-            onDrop
+            onDrop,
+            isDragging
         }
     }
 }
@@ -67,7 +74,7 @@ export default {
 
 <style scoped>
 .drop-zone {
-    background-color: #eee;
+    background-color: #333;
     margin-bottom: 10px;
     padding: 10px;
 }
@@ -76,5 +83,10 @@ export default {
     background-color: #fff;
     margin-bottom: 10px;
     padding: 5px;
+}
+
+.drag-el.dragging {
+    opacity: .5;
+    color: #333;
 }
 </style>
